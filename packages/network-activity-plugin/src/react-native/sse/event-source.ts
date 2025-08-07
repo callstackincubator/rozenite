ddba3986c36f3bd1bdf39dff0d0563ad26c1c850
Szymon Chmal <szymon@chmal.it>
2025-08-07 12:38:52 +0200

    fix(network-activity-plugin): split EventSource to its own chunk

--- packages/network-activity-plugin/src/react-native/sse/event-source.ts
+++ packages/network-activity-plugin/src/react-native/sse/event-source.ts
@@ -17,18 +17,9 @@ export const getEventSource = (): typeof EventSource => {
   try {
     const { default: EventSource } = require('react-native-sse');
     return EventSource;
-  } catch (error) {
-    if (
-      error &&
-      typeof error === 'object' &&
-      'code' in error &&
-      error.code === 'MODULE_NOT_FOUND'
-    ) {
-      // This is a workaround for the fact that Vite doesn't support require() calls for in-project dependencies.
-      // We are going to return a mock object, so the code will work fine, but it will not be able to intercept SSE requests.
-      return MOCK_EVENT_SOURCE;
-    }
-
-    throw error;
+  } catch {
+    // This is a workaround for the fact that Vite doesn't support require() calls for in-project dependencies.
+    // We are going to return a mock object, so the code will work fine, but it will not be able to intercept SSE requests.
+    return MOCK_EVENT_SOURCE;
   }
 };
