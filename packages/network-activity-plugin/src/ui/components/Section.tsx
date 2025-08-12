36d9da34a77e3f5103a2eea9ec0c3b7158566f1d
Nepein Andrey <nepein96@gmail.com>
2025-08-13 00:47:01 +0300

    fix(network-activity-plugin): rewrite cookie parser to match RN specific logic

--- packages/network-activity-plugin/src/ui/components/Section.tsx
+++ packages/network-activity-plugin/src/ui/components/Section.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 export type SectionProps = {
   title: string;
   children: React.ReactNode;
+  childClassName?: string;
 };
 
-export const Section = ({ title, children }: SectionProps) => {
+export const Section = ({ title, children, childClassName }: SectionProps) => {
   return (
     <div>
       <h4 className="text-sm font-medium text-gray-300 mb-2">{title}</h4>
-      {children}
+      <div className={childClassName}>{children}</div>
     </div>
   );
 };
