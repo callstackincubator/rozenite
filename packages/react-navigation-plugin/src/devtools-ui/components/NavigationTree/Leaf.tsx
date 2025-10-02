11e023327a1a67427c86f2f89352ca8facbb50cd
Thomas Renaud <thomasre@bam.tech>
2025-10-02 19:24:59 +0200

    feat: add navigation tree window to the react-navigation plugin

---
+++ packages/react-navigation-plugin/src/devtools-ui/components/NavigationTree/Leaf.tsx
@@ -0,0 +1,33 @@
+export const Leaf = ({
+  title,
+  isSelectedTab,
+  color,
+  subtitle,
+}: {
+  title: string;
+  isSelectedTab?: boolean;
+  color: string;
+  subtitle?: string;
+}) => {
+  return (
+    <div
+      className={`rounded-sm ${
+        isSelectedTab
+          ? `p-1 border-2 border-dashed border-${color}-600`
+          : 'px-1'
+      }`}
+    >
+      <div
+        className={`p-2 text-center rounded-sm bg-${color}-600`}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <span className={`text-gray-100 ${isSelectedTab ? 'underline' : ''}`}>
+          {title}
+        </span>
+        {subtitle ? <span className="text-gray-100">{subtitle}</span> : null}
+      </div>
+    </div>
+  );
+};
