d788b5f8570385db0222ea610dd0b83a75ccef5e
Szymon Chmal <szymonchmal@gmail.com>
2025-11-03 14:35:58 +0100

    fix(redux-devtools-plugin): support Rematch via composeWithRozeniteDevTools (#135)
    Expose an additional composeWithRozeniteDevTools function, which should be used with certain libraries, such as Rematch, to correctly support their async actions.

--- packages/redux-devtools-plugin/react-native.ts
+++ packages/redux-devtools-plugin/react-native.ts
@@ -1,4 +1,5 @@
 export let rozeniteDevToolsEnhancer: typeof import('./src/runtime').rozeniteDevToolsEnhancer;
+export let composeWithRozeniteDevTools: typeof import('./src/runtime').composeWithRozeniteDevTools;
 
 const isWeb =
   typeof window !== 'undefined' && window.navigator.product !== 'ReactNative';
@@ -7,11 +8,39 @@ const isServer = typeof window === 'undefined';
 
 if (isDev && !isWeb && !isServer) {
   rozeniteDevToolsEnhancer = require('./src/runtime').rozeniteDevToolsEnhancer;
+  composeWithRozeniteDevTools =
+    require('./src/runtime').composeWithRozeniteDevTools;
 } else {
-  rozeniteDevToolsEnhancer =
+  // Noop enhancer: returns an enhancer that passes through createStore unchanged
+  const noopEnhancer =
     () =>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (createStore: (...args: any[]) => any) =>
-    (...args) =>
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (...args: any[]) =>
       createStore(...args);
+
+  // Noop composer: returns a compose function (which composes enhancers)
+  const noopComposer = () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (...enhancers: any[]) => {
+      if (enhancers.length === 0) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        return (createStore: (...args: any[]) => any) => createStore;
+      }
+      if (enhancers.length === 1) {
+        return enhancers[0];
+      }
+      // Compose enhancers from right to left (Redux's compose behavior)
+      return enhancers.reduceRight(
+        (composed, enhancer) =>
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          (createStore: (...args: any[]) => any) =>
+            enhancer(composed(createStore))
+      );
+    };
+  };
+
+  rozeniteDevToolsEnhancer = noopEnhancer;
+  composeWithRozeniteDevTools = noopComposer;
 }
