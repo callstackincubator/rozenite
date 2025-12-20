a895e5957d1e02ff3e294a8adbae61547145c7d1
Szymon Chmal <szymonchmal@gmail.com>
2025-12-20 19:51:32 +0100

    refactor(playground): use double-composer

--- apps/playground/metro.config.js
+++ apps/playground/metro.config.js
@@ -23,6 +23,11 @@ const customConfig = {
   cacheVersion: '@rozenite/playground',
   transformer: {
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    getTransformOptions: async () => ({
+      transform: {
+        inlineRequires: false,
+      },
+    }),
   },
   resolver: {
     assetExts: assetExts.filter((ext) => ext !== 'svg'),
@@ -30,32 +35,31 @@ const customConfig = {
     unstable_enablePackageExports: true,
   },
   serializer: {},
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        inlineRequires: false,
-      },
-    }),
-  },
   server: {},
 };
 
-module.exports = withRozenite(
-  withNxMetro(mergeConfig(defaultConfig, customConfig), {
-    // Change this to true to see debugging info.
-    // Useful if you have issues resolving modules
-    debug: false,
-    // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
-    extensions: [],
-    // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
-    // watchFolders: ["../../packages/expo-atlas-plugin"],
-  }),
-  {
-    enabled: true,
-    enhanceMetroConfig: composeMetroConfigTransformers(
-      withRozeniteExpoAtlasPlugin,
-      withRozeniteRequireProfiler,
-      withRozeniteReduxDevTools,
-    ),
-  },
-);
+module.exports = composeMetroConfigTransformers(
+  [
+    withNxMetro,
+    {
+      // Change this to true to see debugging info.
+      // Useful if you have issues resolving modules
+      debug: false,
+      // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
+      extensions: [],
+      // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
+      // watchFolders: ["../../packages/expo-atlas-plugin"],
+    },
+  ],
+  [
+    withRozenite,
+    {
+      enabled: true,
+      enhanceMetroConfig: composeMetroConfigTransformers(
+        withRozeniteExpoAtlasPlugin,
+        withRozeniteRequireProfiler,
+        withRozeniteReduxDevTools,
+      ),
+    },
+  ],
+)(mergeConfig(defaultConfig, customConfig));
