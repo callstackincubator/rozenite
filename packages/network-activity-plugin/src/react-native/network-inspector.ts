1b79a8723ea065f0d75ba6faae56c22bd1fd4a1c
Szymon Chmal <szymon@chmal.it>
2026-01-07 09:26:07 +0100

    refactor: use factory function

--- packages/network-activity-plugin/src/react-native/network-inspector.ts
+++ packages/network-activity-plugin/src/react-native/network-inspector.ts
@@ -5,62 +5,74 @@ import { EventsListener } from './events-listener';
 import { NetworkActivityEventMap } from '../shared/client';
 import type { InspectorsConfig } from './config';
 
-export class NetworkInspector {
-  public readonly http: HTTPInspector;
-  public readonly sse: SSEInspector;
-  public readonly websocket: WebSocketInspector;
-  
-  constructor() {
-    this.http = getHTTPInspector();
-    this.sse = getSSEInspector();
-    this.websocket = getWebSocketInspector();
-  }
+export type NetworkInspector = {
+  readonly http: HTTPInspector;
+  readonly sse: SSEInspector;
+  readonly websocket: WebSocketInspector;
+  setup: (eventsListener: EventsListener<NetworkActivityEventMap>) => void;
+  enable: (config?: InspectorsConfig) => void;
+  disable: () => void;
+  dispose: () => void;
+};
 
-  public setup(eventsListener: EventsListener<NetworkActivityEventMap>) {
-    HTTP_EVENTS.forEach(event => {
-        this.http.on(event, (data) => {
-            eventsListener.send(event, data);
+const createNetworkInspectorInstance = (): NetworkInspector => {
+  const http = getHTTPInspector();
+  const sse = getSSEInspector();
+  const websocket = getWebSocketInspector();
+
+  return {
+    http,
+    sse,
+    websocket,
+
+    setup(eventsListener: EventsListener<NetworkActivityEventMap>) {
+      HTTP_EVENTS.forEach(event => {
+        http.on(event, (data) => {
+          eventsListener.send(event, data);
         });
-    });
+      });
 
-    SSE_EVENTS.forEach(event => {
-        this.sse.on(event, (data) => {
-             eventsListener.send(data.type, data);
+      SSE_EVENTS.forEach(event => {
+        sse.on(event, (data) => {
+          eventsListener.send(data.type, data);
         });
-    });
+      });
 
-    WEBSOCKET_EVENTS.forEach(event => {
-        this.websocket.on(event, (data) => {
-             eventsListener.send(data.type, data);
+      WEBSOCKET_EVENTS.forEach(event => {
+        websocket.on(event, (data) => {
+          eventsListener.send(data.type, data);
         });
-    });
-  }
+      });
+    },
 
-  public enable(config: InspectorsConfig = { http: true, sse: true, websocket: true }) {
-      if (config.http) this.http.enable();
-      if (config.sse) this.sse.enable();
-      if (config.websocket) this.websocket.enable();
-  }
+    enable(config: InspectorsConfig = { http: true, sse: true, websocket: true }) {
+      if (config.http) http.enable();
+      if (config.sse) sse.enable();
+      if (config.websocket) websocket.enable();
+    },
 
-  public disable() {
-      this.http.disable();
-      this.sse.disable();
-      this.websocket.disable();
-  }
+    disable() {
+      http.disable();
+      sse.disable();
+      websocket.disable();
+    },
 
-  public dispose() {
-      this.http.dispose();
-      this.sse.dispose();
-      this.websocket.dispose();
-  }
-}
+    dispose() {
+      http.dispose();
+      sse.dispose();
+      websocket.dispose();
+    },
+  };
+};
 
 export const getNetworkInspector = (() => {
-    let instance: NetworkInspector | null = null;
-    return () => {
-        if (!instance) {
-            instance = new NetworkInspector();
-        }
-        return instance;
-    };
+  let instance: NetworkInspector | null = null;
+  
+  return (): NetworkInspector => {
+    if (!instance) {
+      instance = createNetworkInspectorInstance();
+    }
+    
+    return instance;
+  };
 })();
