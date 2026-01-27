83ee95c6957080563cb4c05cc43d9303ee2e2fdf
crockalet <isse_1997@hotmail.com>
2026-01-27 09:42:23 +0500

    refactor(network-activity-plugin): simplify array check

--- packages/network-activity-plugin/src/react-native/http/http-utils.ts
+++ packages/network-activity-plugin/src/react-native/http/http-utils.ts
@@ -45,30 +45,22 @@ const getTextPostData = (body: unknown): RequestTextPostData => ({
   value: safeStringify(body),
 });
 
-const getFormDataPostData = (body: FormData): RequestFormDataPostData => {
-  const entries = getFormDataEntries(body);
-  const entriesArray = (
-    Array.isArray(entries) ? entries : Array.from(entries || [])
-  ) as Array<[string, unknown]>;
+const getFormDataPostData = (body: FormData): RequestFormDataPostData => ({
+  type: 'form-data',
+  value: Array.from(getFormDataEntries(body)).reduce<
+    RequestFormDataPostData['value']
+  >((acc, [key, value]) => {
+    if (isBlob(value)) {
+      acc[key] = getBinaryPostData(value);
+    } else if (isArrayBuffer(value)) {
+      acc[key] = getArrayBufferPostData(value);
+    } else {
+      acc[key] = getTextPostData(value);
+    }
 
-  return {
-    type: 'form-data',
-    value: entriesArray.reduce<RequestFormDataPostData['value']>(
-      (acc, [key, value]) => {
-        if (isBlob(value)) {
-          acc[key] = getBinaryPostData(value);
-        } else if (isArrayBuffer(value)) {
-          acc[key] = getArrayBufferPostData(value);
-        } else {
-          acc[key] = getTextPostData(value);
-        }
-
-        return acc;
-      },
-      {},
-    ),
-  };
-};
+    return acc;
+  }, {}),
+});
 
 export const getRequestBody = (body: XHRPostData): RequestPostData => {
   if (isNullOrUndefined(body)) {
