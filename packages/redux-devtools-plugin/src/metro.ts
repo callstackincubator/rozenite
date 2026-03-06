34e53f4d9499fe831a5a01d761ea318867989184
Szymon Chmal <szymon@chmal.it>
2026-03-06 12:26:34 +0100

    chore: remove if

--- packages/redux-devtools-plugin/src/metro.ts
+++ packages/redux-devtools-plugin/src/metro.ts
@@ -1,16 +1,11 @@
 import type { ConfigT as MetroConfig } from 'metro-config';
 import { createMetroConfigTransformer } from '@rozenite/tools';
 
-let hasWarned = false;
-
 export const withRozeniteReduxDevTools = createMetroConfigTransformer(
   async (config: MetroConfig): Promise<MetroConfig> => {
-    if (!hasWarned) {
-      hasWarned = true;
-      console.warn(
-        '[Rozenite, redux-devtools] withRozeniteReduxDevTools() is now a no-op and can be safely removed from Metro config.'
-      );
-    }
+    console.warn(
+      '[Rozenite, redux-devtools] withRozeniteReduxDevTools() is now a no-op and can be safely removed from Metro config.'
+    );
 
     return config;
   }
