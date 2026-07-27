b84e43bd3294de3e6483c81d133afd1c275eeddc
Szymon Chmal <szymon@chmal.it>
2026-07-27 14:19:09 +0200

    fix(expo-atlas-plugin): support Metro private serializer paths

--- packages/expo-atlas-plugin/src/base-serializer.ts
+++ packages/expo-atlas-plugin/src/base-serializer.ts
@@ -1,29 +1,96 @@
-const createSerializer = () => {
-  const baseJSBundle = require('metro/src/DeltaBundler/Serializers/baseJSBundle');
-  const bundleToString = require('metro/src/lib/bundleToString');
+import type { ConfigT as MetroConfig } from 'metro-config';
 
-  return (
-    entryPoint: unknown,
-    prepend: unknown,
-    graph: unknown,
-    bundleOptions: unknown
-  ) => bundleToString(baseJSBundle(entryPoint, prepend, graph, bundleOptions));
+type ModuleLoader = {
+  (id: string): unknown;
+  resolve(id: string): string;
 };
 
-export const getBaseSerializer = () => {
+type SerializerModulePaths = {
+  baseJSBundle: string;
+  bundleToString: string;
+};
+
+type BaseSerializer = NonNullable<
+  MetroConfig['serializer']['customSerializer']
+>;
+
+const modernSerializerModulePaths: SerializerModulePaths = {
+  baseJSBundle: 'metro/private/DeltaBundler/Serializers/baseJSBundle',
+  bundleToString: 'metro/private/lib/bundleToString',
+};
+
+const legacySerializerModulePaths: SerializerModulePaths = {
+  baseJSBundle: 'metro/src/DeltaBundler/Serializers/baseJSBundle',
+  bundleToString: 'metro/src/lib/bundleToString',
+};
+
+const isModuleResolutionError = (error: unknown) =>
+  error instanceof Error &&
+  'code' in error &&
+  (error.code === 'MODULE_NOT_FOUND' ||
+    error.code === 'ERR_PACKAGE_PATH_NOT_EXPORTED');
+
+const getUnsupportedMetroError = () =>
+  new Error(
+    'Cannot find required internals of Metro. Please make sure you have installed a supported version of Metro.'
+  );
+
+const unwrapDefaultExport = (module: unknown) => {
+  if (
+    module !== null &&
+    typeof module === 'object' &&
+    'default' in module
+  ) {
+    return module.default;
+  }
+
+  return module;
+};
+
+const resolveModulePaths = (
+  moduleLoader: ModuleLoader,
+  modulePaths: SerializerModulePaths
+) => {
+  moduleLoader.resolve(modulePaths.baseJSBundle);
+  moduleLoader.resolve(modulePaths.bundleToString);
+
+  return modulePaths;
+};
+
+const getSerializerModulePaths = (moduleLoader: ModuleLoader) => {
   try {
-    return createSerializer();
+    return resolveModulePaths(moduleLoader, modernSerializerModulePaths);
   } catch (error) {
-    if (
-      error instanceof Error &&
-      'code' in error &&
-      error.code === 'MODULE_NOT_FOUND'
-    ) {
-      throw new Error(
-        'Cannot find required internals of Metro. Please make sure you have installed the correct version of Metro.'
-      );
+    if (!isModuleResolutionError(error)) {
+      throw error;
+    }
+  }
+
+  return resolveModulePaths(moduleLoader, legacySerializerModulePaths);
+};
+
+export const createBaseSerializer = (moduleLoader: ModuleLoader = require) => {
+  let modulePaths: SerializerModulePaths;
+
+  try {
+    modulePaths = getSerializerModulePaths(moduleLoader);
+  } catch (error) {
+    if (isModuleResolutionError(error)) {
+      throw getUnsupportedMetroError();
     }
 
     throw error;
   }
+
+  const baseJSBundle = unwrapDefaultExport(
+    moduleLoader(modulePaths.baseJSBundle)
+  ) as (...args: Parameters<BaseSerializer>) => unknown;
+  const bundleToString = unwrapDefaultExport(
+    moduleLoader(modulePaths.bundleToString)
+  ) as (bundle: unknown) => ReturnType<BaseSerializer>;
+
+  return (...args: Parameters<BaseSerializer>) =>
+    bundleToString(baseJSBundle(...args));
 };
+
+export const getBaseSerializer = () => createBaseSerializer();
